6eb191c3eaf0bc99d754d8e7b40b712b963eb3ae
rekha0suthar <rekha0suthar@gmail.com>
2024-12-23 18:44:30 +0530

    api for card - block/unblock, generate pin and new card

--- backend/controllers/account.js
+++ backend/controllers/account.js
@@ -45,4 +45,62 @@ const accountDetails = async (req, res) => {
   }
 };
 
-export { getBalance, accountDetails };
+const getDebitCard = async (req, res) => {
+  try {
+    const { userId } = req.params;
+    const userDebitCard = await Account.findOne({ userId }, { debitCard: 1 });
+
+    if (!userDebitCard) {
+      return res.status(404).json({ msg: 'Account not found' });
+    }
+    res.status(200).json(userDebitCard);
+  } catch (err) {
+    res.status(500).json({ msg: err.message });
+  }
+};
+
+const blockUnblockDebitCart = async (req, res) => {
+  try {
+    const { debitCard } = req.body;
+
+    const account = await Account.findOne({ debitCard });
+
+    if (!account) {
+      return res.status(404).json({ message: 'Debit card not found' });
+    }
+    if (account.debitCard.blocked) {
+      account.debitCard.blocked = false;
+      await account.save();
+      return res.status(200).json({ msg: 'Card unblocked' });
+    }
+    account.debitCard.blocked = true;
+    await account.save();
+    res.status(200).json({ msg: 'Card blocked' });
+  } catch (err) {
+    res.status(500).json({ message: 'Internal server error' });
+  }
+};
+
+const generateCardPin = async (req, res) => {
+  const { debitCard, pin } = req.body;
+  try {
+    const account = await Account.findOne({ debitCard });
+    if (!account.debitCard) {
+      return res.status(404).json({ message: 'Debit card not found' });
+    }
+    account.debitCard.pin = pin;
+    account.debitCard.active = true;
+    await account.save();
+    res.status(200).json({ msg: 'Card pin generated' });
+  } catch (err) {
+    res.status(500).json({ message: 'Internal server error' });
+  }
+};
+
+export {
+  getBalance,
+  getDebitCard,
+  accountDetails,
+  blockUnblockDebitCart,
+  generateCardPin,
+};
--- backend/routes/account.js
+++ backend/routes/account.js
@@ -1,9 +1,19 @@
 import { Router } from 'express';
 import { verifyToken } from '../middleware/verifyToken.js';
-import { getBalance } from '../controllers/account.js';
+import {
+  accountDetails,
+  getBalance,
+  getDebitCard,
+  blockUnblockDebitCart,
+  generateCardPin,
+} from '../controllers/account.js';
 
 const router = Router();
 
 router.get('/:userId/balance', verifyToken, getBalance);
+router.get('/:userId/debit-card', verifyToken, getDebitCard);
+router.get('/:userId/account-summary', verifyToken, accountDetails);
+router.post('/block-unblock-card', verifyToken, blockUnblockDebitCart);
+router.post('/generate-pin', verifyToken, generateCardPin);
 
 export default router;
